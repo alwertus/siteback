6a49f2c29b46f801b8fc283a30ce89a9a389f0c9
Alwertus <alwertus@yandex.ru>
2019-10-10 17:49:51 +0300

    Little changes

--- src/main/java/ru/alwertus/siteback/servlets/MenuItemsServlet.java
+++ src/main/java/ru/alwertus/siteback/servlets/MenuItemsServlet.java
@@ -1,10 +1,25 @@
 package ru.alwertus.siteback.servlets;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.json.JSONObject;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class MenuItemsServlet extends HttpServlet implements IServlet  {
+
+    private static Logger log = LogManager.getLogger(MenuItemsServlet.class.getName());
+    @Override public String getURL() { return "/menuitems"; }
+
+    // get POST message
     @Override
-    public String getURL() {
-        return "/menuitems";
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+//        JSONObject json = new JSONObject();
+        String opt = request.getParameter("option");
+        log.trace(opt);
     }
 }
