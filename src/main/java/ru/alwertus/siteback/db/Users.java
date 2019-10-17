c5d23b28da3d96d1c64e05e0e9d85e8ce5b552aa
Alwertus <alwertus@yandex.ru>
2019-10-17 18:02:35 +0300

    little db changes

--- src/main/java/ru/alwertus/siteback/db/Users.java
+++ src/main/java/ru/alwertus/siteback/db/Users.java
@@ -7,7 +7,7 @@ public class Users {
 
     // constructor
     public Users() {
-        SQL.put("users_createtable",
+        SQL.put("1.0",
                 "CREATE TABLE IF NOT EXISTS USERS ( " + String.join(", ", new String[]{
                         "row_id INT NOT NULL AUTO_INCREMENT",
                         "name VARCHAR(30)",
@@ -18,27 +18,34 @@ public class Users {
                         "sessionkey VARCHAR(20)",
                         "PRIMARY KEY(row_id)"
                 }) +");");
-        SQL.put("roles_createtable",
+
+        SQL.put("2.0",
                 "CREATE TABLE IF NOT EXISTS ROLES ( " + String.join(", ", new String[]{
                         "row_id INT NOT NULL AUTO_INCREMENT",
                         "name VARCHAR(30)",
                         "PRIMARY KEY(row_id)"
                 }) +");");
-        SQL.put("users-roles-int_createtable",
+        SQL.put("2.1", "INSERT IGNORE INTO ROLES (row_id, name) VALUES ('1','owner'),('2','admin'),('3','user'),('4','none');");
+
+        SQL.put("3.0",
                 "CREATE TABLE IF NOT EXISTS USR_ROLE_INT ( " + String.join(", ", new String[]{
                         "row_id INT NOT NULL AUTO_INCREMENT",
                         "user_id INT",
                         "role_id INT",
                         "PRIMARY KEY(row_id)"
                 }) +");");
+        SQL.put("3.1", "INSERT IGNORE INTO USR_ROLE_INT (row_id, name) VALUES ('1','owner'),('2','admin'),('3','user'),('4','none');");
 
         init();
     }
 
     public void init() {
-        DB.executeSQL(SQL.get("users_createtable"));
-        DB.executeSQL(SQL.get("roles_createtable"));
-        DB.executeSQL(SQL.get("users-roles-int_createtable"));
+        DB.executeSQL(SQL.get("1.0"));
+        DB.executeSQL(SQL.get("2.0"));
+        DB.executeSQL(SQL.get("2.1"));
+        DB.executeSQL(SQL.get("3.0"));
+        DB.executeSQL(SQL.get("3.1"));
+
     }
 
 }
